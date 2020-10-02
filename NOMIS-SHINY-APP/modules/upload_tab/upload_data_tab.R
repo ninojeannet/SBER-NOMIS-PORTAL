8430ca140553ce1d9e52bb4b756b3c1c6a95dad1
ninojeannet <ninjea23@gmail.com>
2020-10-02 12:19:53 +0200

    prepare file upload method

--- NOMIS-SHINY-APP/modules/upload_tab/upload_data_tab.R
+++ NOMIS-SHINY-APP/modules/upload_tab/upload_data_tab.R
@@ -16,7 +16,8 @@ uploadDataTabUI <- function(id){
     sidebarPanel(
       id = ns('sidebar-upload'),
       div(
-        selectInput(ns("type"),label = "Select a data type",choices = c("Glacier" = "glacier","Location"="location","Patch"="patch","Enzyme"="enzyme")),
+        selectInput(ns("type"),label = "Select a data type",choices = c("Glacier" = "glacier","Location"="location","Patch"="patch","Enzyme"="enzyme","DOM"="dom")),
+        hidden(selectInput(ns("dom-type"),label = "Select a DOM parameter",choices = c("EEM" = "eem","Absorbance 1cm"="abs1","Absorbance 10cm"="abs10"))),
         radioButtons(ns("selectRange"), "Choose a selection option :",
                      c("Unique glacier" = "simple",
                      "Range of glacier" = "range",
@@ -25,6 +26,7 @@ uploadDataTabUI <- function(id){
         hidden(numericRangeInput(ns("glacierRange"),label = "Glacier range", value = c(1, 500))),
         hidden(textInput(ns("glacierList"),"Glacier list (comma separated)")),
         actionButton(ns("generate"),"Generate template"),
+        hidden( fileInput(ns("files"),"Select files",accept=".csv",multiple = TRUE))
       ),
       width = 4
     ),
@@ -32,8 +34,14 @@ uploadDataTabUI <- function(id){
     mainPanel(
       id = ns('main-upload'),
       div(
+        div(class = "file-output",
+          div(class="file-table",tableOutput(ns("fileValid"))),
+          div(class="file-table",tableOutput(ns("fileWrong"))),
+          div(class="file-table",tableOutput(ns("fileMissing")))
+        ),
         rHandsontableOutput(ns("table")),
-        hidden(actionButton(ns("upload"),"Upload data",class="upload-button"))
+        hidden(actionButton(ns("upload"),"Upload data",class="upload-button")),        
+        hidden(actionButton(ns("uploadFiles"),"Upload valid files",class="upload-button"))
       ),
       width = 8
     )
@@ -126,6 +134,59 @@ uploadDataTab <- function(input,output,session,pool,dimension){
   # res <-observeEvent(input$hot_select,{
   #   print("test")
   # })
+  observeEvent(input$type,{
+    switch (input$type,
+      "dom" = {
+        showElement("dom-type")
+        showElement("files")
+        showElement("uploadFiles")
+        hideElement("generate")
+        hideElement("table")
+        
+      },
+      {
+        hideElement("dom-type")
+        hideElement("files")
+        hideElement("uploadFiles")
+        
+        showElement("generate")
+        showElement("table")
+        
+      }
+    )
+  })
+  
+  observeEvent(input$files,{
+    files <- input$files
+    print(files)
+    switch (input$selectRange,
+            "simple" = {
+              ids <- input$glacier
+            },
+            "range" = {
+              range <- input$glacierRange
+              ids <- c(paste0("GL",as.character(range[1]:range[2])))
+            },
+            "list" = {
+              ids <- input$glacierList
+              ids <- gsub(" ", "", ids, fixed = TRUE)
+              ids <- strsplit(ids,',')
+              ids <- sapply(ids, function(x){paste0("GL",x)})
+            }
+    
+    )
+    filenames <- generateFilenames(ids)
+    print(filenames)
+    tables <- generateFileTables(filenames,files)
+    output$fileValid <- renderTable({ data.frame("Valid" =tables[["valid"]])})
+    output$fileWrong <- renderTable({ data.frame("Wrong" =tables[["wrong"]])})
+    output$fileMissing <- renderTable({ data.frame("Missing" =tables[["missing"]])})
+    
+    # ext <- tools::file_ext(files$datapath)
+    # 
+    # req(files)
+
+  })
   
   observeEvent(input$upload,{
     out <- hot_to_r(input$table)
@@ -153,6 +214,41 @@ uploadDataTab <- function(input,output,session,pool,dimension){
 #   # }
 #   return(rows)
 # }
+
+generateFilenames <- function(ids){
+  filenames <- vector()
+  for (id in ids) {
+    f1 <- paste0(id,"UP_1_dom.dat")
+    f2 <- paste0(id,"UP_2_dom.dat")
+    f3 <- paste0(id,"UP_3_dom.dat")
+    
+    filenames<-c(filenames,f1)
+    filenames<-c(filenames,f2)
+    filenames<-c(filenames,f3)
+    
+  }
+  return(filenames)
+}
+
+generateFileTables <- function(filenames,files){
+  tables <- list()
+  tables[["valid"]] <- vector()
+  tables[["wrong"]] <- vector()
+  tables[["missing"]] <- vector()
+  for (filename in filenames) {
+    if (filename %in% files[["name"]])
+     tables[["valid"]] <- c(tables[["valid"]],filename)
+    else
+      tables[["missing"]] <- c(tables[["missing"]],filename)
+  }
+  for (val in files[["name"]]) {
+    if(!(val %in% tables[["valid"]]) & !(val %in% tables[["missing"]]))
+      tables[["wrong"]] <- c(tables[["wrong"]],val)
+    
+  }
+  
+  return(tables)
+}
 
 # Check if the input format are valid and if not display a message
 # Parameters :
