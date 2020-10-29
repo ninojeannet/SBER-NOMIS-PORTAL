f3d725ad1787e5015decde4fd5069749d0c584e2
ninojeannet <ninjea23@gmail.com>
2020-10-29 09:36:33 +0100

    add packages installation file

---
+++ NOMIS-SHINY-APP/packages_installation.R
@@ -0,0 +1,34 @@
+install.packages(
+  pkgs = c("shiny", 
+           "shinyjs", 
+           "shinyWidgets", 
+           "shinybusy", 
+           "shinycssloaders", 
+           "sass", 
+           "jsonlite", 
+           "readr", 
+           "stringr", 
+           "ggplot2", 
+           "Cairo", 
+           "data.table", 
+           "lubridate", 
+           "forcats", 
+           "tidyr", 
+           "magrittr", 
+           "dplyr",
+           "DBI",
+           "RMySQL",
+           "pool",
+           "dbplyr",
+           "sodium",
+           "DT",
+           "rhandsontable",
+           "lambda.r",
+           "rlist",
+           "formattable",
+           "DataCombine"
+  ),
+  repos = 'https://cran.rstudio.com/'
+)
+
+remotes::install_github("rstudio/shinyvalidate")
