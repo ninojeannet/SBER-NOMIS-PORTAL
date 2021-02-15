a5db6c4a27052b4eecc92509816f7a4236ac005c
ninojeannet <ninjea23@gmail.com>
2021-02-15 15:44:33 +0100

    add basic iframe for map visualisation

--- modules/visualisation_tab/visualisation_tab.R
+++ modules/visualisation_tab/visualisation_tab.R
@@ -1,7 +1,6 @@
 source('./modules/visualisation_tab/visualisation_versus_tab.R')
 source('./modules/visualisation_tab/visualisation_simple_tab.R')
 
-
 visualisationTabUI <- function(id,pool) {
   # Parameters:
   #  - id: String, the module id
@@ -15,7 +14,15 @@ visualisationTabUI <- function(id,pool) {
   tabPanel(
       'Visualisation simple',
       visualisationSimpleTabUI(ns("simple"),pool)
-    )
+    ),
+  # And the weather tab
+  tabPanel(
+    # Tab title
+    'Map',
+    # Tab content
+    htmlTemplate('./html_components/map.html'),
+    value = ns('mapTab')
+  )
   )
   # div(
   #   "functionality not available yet..."
@@ -25,4 +32,5 @@ visualisationTabUI <- function(id,pool) {
 visualisationTab <- function(input, output, session,pool){
   callModule(visualisationVersusTab,"versus",pool)
   callModule(visualisationSimpleTab,"simple",pool)
+
 }
