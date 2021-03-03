4a21ec5dfc649d41207e8bdc76c3d68d274916c9
ninojeannet <ninjea23@gmail.com>
2021-03-03 13:55:21 +0100

    fix map refresh

--- modules/visualisation_tab/visualisation_tab.R
+++ modules/visualisation_tab/visualisation_tab.R
@@ -12,7 +12,8 @@ visualisationTabUI <- function(id,pool) {
       # Tab title
       'Map',
       # Tab content
-      htmlTemplate('./html_components/map.html',url = getMapURL()),
+      htmlTemplate('./html_components/map.html',btn=div(class= 'main-inputs',div(class = 'main-actions',actionButton(ns('refresh'), 'Refresh', class = 'help custom-style custom-style--primary')))
+        ,frame=uiOutput(ns("map"))),
       value = ns('mapTab')
     ),
     tabPanel(
@@ -29,5 +30,16 @@ visualisationTabUI <- function(id,pool) {
 visualisationTab <- function(input, output, session,pool){
   callModule(visualisationVersusTab,"versus",pool)
   callModule(visualisationSimpleTab,"simple",pool)
+  
+  observeEvent(input$refresh,{
+    
+    output$map <- renderUI({
+      tags$iframe(class="mapframe", frameborder="0", scrolling="no", marginheight="0", marginwidth="0", title="NOMIS Map", src=getMapURL())
+    })
+  })
+  
+ output$map <- renderUI({
+   tags$iframe(class="mapframe", frameborder="0", scrolling="no", marginheight="0", marginwidth="0", title="NOMIS Map", src=getMapURL())
+ })
 
 }
